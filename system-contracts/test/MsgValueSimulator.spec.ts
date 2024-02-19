41af2caeafedcb5a8df2e3620a3a725f2bbdd446
maciek.nabialek <maciej.nabialek@blockydevs.com>
2024-02-19 19:07:22 +0100

    changes

--- system-contracts/test/MsgValueSimulator.spec.ts
+++ system-contracts/test/MsgValueSimulator.spec.ts
@@ -1,5 +1,5 @@
 import type { Wallet } from "zksync-web3";
-import {MsgValueSimulator, MsgValueSimulatorFactory} from "../typechain";
+import {MockContract, MsgValueSimulator, MsgValueSimulatorFactory} from "../typechain";
 import {deployContract, deployContractOnAddress, getWallets } from "./shared/utils";
 import {
     TEST_MSG_VALUE_SYSTEM_CONTRACT_ADDRESS
@@ -32,21 +32,35 @@ describe("L2EthToken tests", () => {
         it("should get reverted if called by itself", async () => {
             await expect(
                 messageValueSimulatorSystemCall.fallback({data: encodeExtraAbiCallerCalldata(
-                                    messageValueSimulatorSystemCall.address,
-                                    BigNumber.from(0),
-                                    ["0"],
-                                    "0x"
+                                    messageValueSimulator.address,
+                                    BigNumber.from(messageValueSimulator.address),
+                                    [
+                                        '0x1111111111111111111111111111111111111111111111111111111111111111',
+                                        '0x2222222222222222222222222222222222222222222222222222222222222222',
+                                        '0x3333333333333333333333333333333333333333333333333333333333333333',
+                                        '0x4444444444444444444444444444444444444444444444444444444444444444',
+                                        '0x5555555555555555555555555555555555555555555555555555555555555555',
+                                        '0x6666666666666666666666666666666666666666666666666666666666666666',
+                                        '0x7777777777777777777777777777777777777777777777777777777777777777',
+                                        '0x8888888888888888888888888888888888888888888888888888888888888888',
+                                        '0x9999999999999999999999999999999999999999999999999999999999999999',
+                                        '0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                                    ],
+                                    '0x'
                                 )})
             )
-                .to.emit(messageValueSimulatorSystemCall, 'StartOfTheTest')
+                .to.emit(messageValueSimulator, 'StartOfTheTest')
                 .withArgs(true)
-
+                .to.emit(messageValueSimulator, 'AbiParams')
+                .withArgs(BigInt(0), false, messageValueSimulator.address)
+                .to.be.revertedWith("MsgValueSimulator calls itself");
         });
 
 
-    //     it("should attempt to transfer token to recipient if value is non-zero", async () => {
-    //
-    //     });
+        // it("should attempt to transfer token to recipient if value is non-zero", async () => {
+
+        // });
+
     //
     //     it("should revert if transfer fails", async () => {
     //
