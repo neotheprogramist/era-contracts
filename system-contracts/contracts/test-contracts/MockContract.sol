84ceb62cf08d999a772f73c93740a82b988ec2b5
Mateusz Zając <matzayonc@gmail.com>
2024-02-29 13:30:09 +0100

    test with pass, no transfer yet

--- system-contracts/contracts/test-contracts/MockContract.sol
+++ system-contracts/contracts/test-contracts/MockContract.sol
@@ -32,37 +32,37 @@ contract MockContract {
 
     fallback() external payable {
         bytes memory data = msg.data;
-        bytes32 inputKeccak = keccak256(data);
+        // bytes32 inputKeccak = keccak256(data);
 
-        // empty return data with successful result by default.
-        bool failure;
-        bytes memory returnData;
+        // // empty return data with successful result by default.
+        // bool failure;
+        // bytes memory returnData;
 
-        for (uint256 i = 0; i < results.length; i++) {
-            if (keccak256(results[i].input) == inputKeccak) {
-                failure = results[i].failure;
-                returnData = results[i].returnData;
-                break;
-            }
-        }
+        // for (uint256 i = 0; i < results.length; i++) {
+        //     if (keccak256(results[i].input) == inputKeccak) {
+        //         failure = results[i].failure;
+        //         returnData = results[i].returnData;
+        //         break;
+        //     }
+        // }
 
-        // Emitting event only if empty successful result expected.
-        // Can fail if call context is static, but usually it's not a case,
-        // because view/pure call without return data doesn't make sense.
-        // Useful, because for such calls we can check for this event,
-        // to be sure that the needed call was made.
-        if (!failure && returnData.length == 0) {
+        // // Emitting event only if empty successful result expected.
+        // // Can fail if call context is static, but usually it's not a case,
+        // // because view/pure call without return data doesn't make sense.
+        // // Useful, because for such calls we can check for this event,
+        // // to be sure that the needed call was made.
+        // if (!failure && returnData.length == 0) {
             emit Called(msg.value, data);
-        }
+        // }
 
-        assembly {
-            switch failure
-            case 0 {
-                return(add(returnData, 0x20), mload(returnData))
-            }
-            default {
-                revert(add(returnData, 0x20), mload(returnData))
-            }
-        }
+        // assembly {
+        //     switch failure
+        //     case 0 {
+        //         return(add(returnData, 0x20), mload(returnData))
+        //     }
+        //     default {
+        //         revert(add(returnData, 0x20), mload(returnData))
+        //     }
+        // }
     }
 }
--- system-contracts/test/MsgValueSimulator.spec.ts
+++ system-contracts/test/MsgValueSimulator.spec.ts
@@ -1,4 +1,4 @@
-import { MsgValueSimulator, MsgValueSimulatorFactory } from "../typechain";
+import { MockContract, MsgValueSimulator, MsgValueSimulatorFactory } from "../typechain";
 import { deployContract, deployContractOnAddress, getWallets, loadZasmBytecode, setCode } from "./shared/utils";
 import { TEST_ETH_TOKEN_SYSTEM_CONTRACT_ADDRESS, TEST_MSG_VALUE_SYSTEM_CONTRACT_ADDRESS } from "./shared/constants";
 import { prepareEnvironment } from "./shared/mocks";
@@ -10,18 +10,16 @@ import { Contract } from "zksync-web3";
 describe("MsgValueSimulator tests", () => {
   let messageValueSimulator: MsgValueSimulator;
   let extraAbiCaller: Contract;
-  let systemCaller: MsgValueSimulator;
+  let mockContract: MockContract;
 
   before(async () => {
     await prepareEnvironment();
     const deployerWallet = getWallets()[0];
     await deployContractOnAddress(TEST_MSG_VALUE_SYSTEM_CONTRACT_ADDRESS, "MsgValueSimulator");
 
+    mockContract = (await deployContract("MockContract")) as MockContract;
+
     messageValueSimulator = MsgValueSimulatorFactory.connect(TEST_MSG_VALUE_SYSTEM_CONTRACT_ADDRESS, deployerWallet);
-    const messageValueSimulatorSystemCallContract = await deployContract("SystemCaller", [
-      messageValueSimulator.address,
-    ]);
-    systemCaller = MsgValueSimulatorFactory.connect(messageValueSimulatorSystemCallContract.address, deployerWallet);
     const extraAbiCallerBytecode = await loadZasmBytecode("ExtraAbiCaller", "test-contracts");
     await setCode(EXTRA_ABI_CALLER_ADDRESS, extraAbiCallerBytecode);
     extraAbiCaller = new Contract(EXTRA_ABI_CALLER_ADDRESS, [], deployerWallet);
@@ -31,11 +29,18 @@ describe("MsgValueSimulator tests", () => {
     it("should pass and emit", async () => {
       await expect(
         extraAbiCaller.fallback({
-          data: encodeExtraAbiCallerCalldata(messageValueSimulator.address, BigNumber.from(0), ["5"], "0x01"),
+          data: encodeExtraAbiCallerCalldata(
+            messageValueSimulator.address,
+            BigNumber.from(0),
+            ["5", mockContract.address, "0"],
+            "0x01"
+          ),
         })
       )
-        .to.emit(messageValueSimulator, "AbiParams")
-        .withArgs(5, true, "0x0000000000000000000000000000000000000002");
+        // .to.emit(messageValueSimulator, "AbiParams")
+        // .withArgs(5, false, mockContract.address);
+        .to.emit(mockContract, "Called")
+        .withArgs(5, "0x01");
     });
   });
 });
