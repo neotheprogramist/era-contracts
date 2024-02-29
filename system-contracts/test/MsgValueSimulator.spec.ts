72224a67368f37592ee02ee6a06b1d12f93b6367
Mateusz Zając <matzayonc@gmail.com>
2024-02-29 12:31:10 +0100

    passed data to mvs

--- system-contracts/test/MsgValueSimulator.spec.ts
+++ system-contracts/test/MsgValueSimulator.spec.ts
@@ -1,5 +1,5 @@
 import { MsgValueSimulator, MsgValueSimulatorFactory } from "../typechain";
-import { deployContract, deployContractOnAddress, getWallets } from "./shared/utils";
+import { deployContract, deployContractOnAddress, getWallets, loadZasmBytecode, setCode } from "./shared/utils";
 import { TEST_ETH_TOKEN_SYSTEM_CONTRACT_ADDRESS, TEST_MSG_VALUE_SYSTEM_CONTRACT_ADDRESS } from "./shared/constants";
 import { prepareEnvironment } from "./shared/mocks";
 import { expect } from "chai";
@@ -22,34 +22,20 @@ describe("MsgValueSimulator tests", () => {
       messageValueSimulator.address,
     ]);
     systemCaller = MsgValueSimulatorFactory.connect(messageValueSimulatorSystemCallContract.address, deployerWallet);
+    const extraAbiCallerBytecode = await loadZasmBytecode("ExtraAbiCaller", "test-contracts");
+    await setCode(EXTRA_ABI_CALLER_ADDRESS, extraAbiCallerBytecode);
     extraAbiCaller = new Contract(EXTRA_ABI_CALLER_ADDRESS, [], deployerWallet);
   });
 
   describe("fallback", () => {
     it("should pass and emit", async () => {
       await expect(
-        systemCaller.fallback({
-          data: encodeExtraAbiCallerCalldata(
-            messageValueSimulator.address,
-            BigNumber.from(1),
-            [
-              "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
-              "0x2222222222222222222222222222222222222222222222222222222222222222",
-              "0x3333333333333333333333333333333333333333333333333333333333333333",
-              "0x4444444444444444444444444444444444444444444444444444444444444444",
-              "0x5555555555555555555555555555555555555555555555555555555555555555",
-              "0x6666666666666666666666666666666666666666666666666666666666666666",
-              "0x7777777777777777777777777777777777777777777777777777777777777777",
-              "0x8888888888888888888888888888888888888888888888888888888888888888",
-              "0x9999999999999999999999999999999999999999999999999999999999999999",
-              "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            ],
-            "0x01"
-          ),
+        extraAbiCaller.fallback({
+          data: encodeExtraAbiCallerCalldata(messageValueSimulator.address, BigNumber.from(0), ["5"], "0x01"),
         })
       )
         .to.emit(messageValueSimulator, "AbiParams")
-        .withArgs(1, true, "0x0000000000000000000000000000000000000002");
+        .withArgs(5, true, "0x0000000000000000000000000000000000000002");
     });
   });
 });
