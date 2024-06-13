ed13ef4c182e343b92f9126b5c824dbcb3b82877
tejks <sarmatys.programmer@gmail.com>
2024-06-13 16:16:47 +0200

    lint fix

--- l1-contracts/test/foundry/integration/deploy-scripts/Utils.sol
+++ l1-contracts/test/foundry/integration/deploy-scripts/Utils.sol
@@ -9,7 +9,7 @@ library Utils {
     // Cheatcodes address, 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D.
     address internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
     Vm internal constant vm = Vm(VM_ADDRESS);
- 
+
     function executeUpgrade(
         address _governor,
         bytes32 _salt,
